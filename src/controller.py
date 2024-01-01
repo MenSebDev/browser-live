4cc6391fff9ad39063e3c81f8cc1f433510bb7d5
Sébastien Menard <sebastien.menard.github@gmail.com>
2024-01-01 13:16:48 -0500

    feat(controller): create controller logic

---
+++ src/controller.py
@@ -0,0 +1,37 @@
+"""Module Controller."""
+
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from utilities import PATH_DRIVERS, Drivers
+
+from tools import logger
+
+if TYPE_CHECKING:
+    from pathlib import Path
+
+    from selenium.webdriver.chrome.webdriver import WebDriver
+
+
+class Controller:
+    """Interface representing the controller."""
+
+    def __init__(self: Controller, path_drivers: Path) -> None:
+        self.browsers = {}
+        self.drivers = Drivers(path_drivers=path_drivers)
+
+    def driver(
+        self: Controller,
+        browser: str,
+    ) -> WebDriver:
+        if browser not in self.browsers:
+            driver = self.drivers[browser]
+            self.browsers[browser] = driver()
+
+        return self.browsers.get(browser)
+
+
+logger.info_(msg=f"PATH {PATH_DRIVERS}")
+
+controller = Controller(path_drivers=PATH_DRIVERS)
